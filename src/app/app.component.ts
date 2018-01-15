603d65216b2b196d9b833e9a75a355020615f729
anthony <awwagler@gmail.com>
2018-01-14 22:04:06 -0600

    changed home page pic and title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  private homePageImage = require("./images/original.jpg")
+  private homePageImage = require("./images/cars.jpg");
 }
